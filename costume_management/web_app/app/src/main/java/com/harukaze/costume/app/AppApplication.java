e2f0eeea4554c16ab80553d670554e191fdb3c90
dokidokikoi <h18673628658@outlook.com>
2022-06-01 08:26:23 +0800

    后端项目初始化

---
+++ costume_management/web_app/app/src/main/java/com/harukaze/costume/app/AppApplication.java
@@ -0,0 +1,19 @@
+package com.harukaze.costume.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @PackageName: com.harukaze.app
+ * @ClassName: AppApplication
+ * @Description:
+ * @Author: doki
+ * @Date: 2022/5/30 8:56
+ */
+@SpringBootApplication
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+}
